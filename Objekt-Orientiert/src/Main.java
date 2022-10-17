2c1d528db8d6c54a587867164a5da933074df67c
Your Name <you@example.com>
2022-10-17 16:08:56 +0200

    03

--- Objekt-Orientiert/src/Main.java
+++ Objekt-Orientiert/src/Main.java
@@ -1,4 +1,4 @@
-import _02_Das_Erste_objekt.Car;
+import _02_Das_Erste_Objekt.Car;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,13 +7,21 @@ public class Main {
         c1.brand = "Audi";
         c1.fuelConsumption  = 7;
         c1.serialNumber = "A1234";
+        c1.fuelAmount = 55;
+        c1.tankAmount = 60;
 
         Car c2 = new Car();
         c2.brand = "BMW";
         c2.fuelConsumption  = 8;
         c2.serialNumber = "B1234";
+        c2.fuelAmount  = 12;
+        c2.tankAmount = 80;
 
-        System.out.println(c1.serialNumber);
-        System.out.println(c2.serialNumber);
+        System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
+        c1.Break();
+        c1.turboBoost();
+        c2.turboBoost();
     }
 }
--- Objekt-Orientiert/src/_02_Das_Erste_Objekt/Car.java
+++ Objekt-Orientiert/src/_02_Das_Erste_Objekt/Car.java
@@ -1,11 +1,30 @@
-package _02_Das_Erste_objekt;
+package _02_Das_Erste_Objekt;
 
 public class Car {
     // Instanz, Gedächnisvariablen
-
-    // don't do that later
     public int fuelConsumption;
+    public int fuelAmount;
+    public int tankAmount;
     public String brand;
     public String serialNumber;
     private String color;
+
+    // Methode
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption ;
+        System.out.println("I am driving.");
+    }
+
+    public void Break(){
+        System.out.println("Ich break.");
+    }
+
+    public void turboBoost(){
+        if(this.fuelAmount > this.tankAmount/10){
+            System.out.println("SuperBoostMode on.");
+        }
+        else{
+            System.out.println("Not enough fuel to enable SuperBoostMode.");
+        }
+    }
 }
